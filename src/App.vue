9beeb6da89da6e3291cec65099f17f614d29135c
ivania <ivaniaespejoduran@gmail.com>
2024-09-03 16:13:46 -0400

    My first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <Navbar />
+
+  <router-view />
+
+  <medios-pago />
+  <Footer />
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+import FooTer from '@/components/FooTer.vue'
+import MediosPago from '@/components/MediosPago.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    NavBar,
+    FooTer,
+    MediosPago
+  }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <CaRd />
+</template>
+
+<script>
+// @ is an alias to /src
+import CaRd from '@/components/CaRd.vue'
+
+export default {
+  components: {
+    CaRd
+  }
+}
+</script>
